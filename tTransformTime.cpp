61e5daf3d4665f4e0bf7f983a185419ee745df3e
Tobias Föhst <foehst@informatik.uni-kl.de>
2013-05-05 23:19:03 +0200

    Equalized preamble

--- tTransformTime.cpp
+++ tTransformTime.cpp
@@ -1,28 +1,28 @@
 //
-// You received this file as part of MCA2
-// Modular Controller Architecture Version 2
+// You received this file as part of RRLib
+// Robotics Research Library
 //
-//Copyright (C) Robotics Research Lab (RRLab) University of Kaiserslautern
+// Copyright (C) Finroc GbR (finroc.org)
 //
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either version 2
-//of the License, or (at your option) any later version.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
 //
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //
-// this is for emacs file handling -*- mode: c++; indent-tabs-mode: nil -*-
 //----------------------------------------------------------------------
 /*!\file    tTransformTime.cpp
  *
  * \author  Bernd Helge Schaefer
+ *
  * \date    06.06.2006
  *
  */
--- type_list/tMostDerived.h
+++ type_list/tMostDerived.h
@@ -6,17 +6,17 @@
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
-// as published by the Free Software FoundtMostDerivedion; either version 2
-// of the License, or (tMostDerived your option) any ltMostDeriveder version.
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
 //
-// This program is distributed in the hope thtMostDerived it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
-// FoundtMostDerivedion, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //
 //----------------------------------------------------------------------
 /*!\file    tMostDerived.h

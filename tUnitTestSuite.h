492348950c4823e519f328b5a2fd387a6ee58452
Michael Arndt <m_arndt@cs.uni-kl.de>
2013-05-28 09:21:48 +0200

    Added wrapper to CPPUNIT_ASSERT_DOUBLES_EQUAL which can be used to check equality for double values with given delta

--- tUnitTestSuite.h
+++ tUnitTestSuite.h
@@ -66,6 +66,7 @@ namespace util
 
 #define RRLIB_UNIT_TESTS_ASSERT(expression) CPPUNIT_ASSERT(expression)
 #define RRLIB_UNIT_TESTS_EQUALITY(a, b) CPPUNIT_ASSERT_EQUAL(a, b)
+#define RRLIB_UNIT_TESTS_EQUALITY_DOUBLE(a, b, delta) CPPUNIT_ASSERT_DOUBLES_EQUAL(a, b, delta)
 #define RRLIB_UNIT_TESTS_EXCEPTION(expression, exception_type) CPPUNIT_ASSERT_THROW(expression, exception_type)
 
 //----------------------------------------------------------------------
